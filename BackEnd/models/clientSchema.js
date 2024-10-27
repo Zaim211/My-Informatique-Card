f466a5c31d7d23bf3e7c9e74f0b811535c0db0b7
abc <abc@example.com>
2024-10-27 21:20:30 +0000

    update

--- BackEnd/models/clientSchema.js
+++ BackEnd/models/clientSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const clientSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
   verificationToken: { type: String },
