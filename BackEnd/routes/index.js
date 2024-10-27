6c079574d119d51a72c8d7f0af3e4ca844c1cb5c
abc <abc@example.com>
2024-10-27 21:37:25 +0000

    update

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -6,6 +6,9 @@ const router = Router();
 router.get("/", AppController.MyInfoCard);
 router.post('/register', AuthenticationController.register);
 router.get('/verify/:token', AuthenticationController.verify);
+// router.get('/redirect', AuthenticationController.redirectToPortfolioOrSetup);
+// router.get('/generate-qr', AuthenticationController.getQRCode);
+router.post('/login', AuthenticationController.login);
 
 router.post('/scanForm/:userId', AuthenticationController.scanFormClient);
 router.put('/scanForm/:id', AuthenticationController.updateForm)
